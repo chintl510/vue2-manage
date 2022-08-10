718283213b220a092401df545e154edac2041cd5
chintl510 <66775442+chintl510@users.noreply.github.com>
2022-08-10 14:11:50 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/vue2-management-platform/', //为gh-pages设置，服务器配置可去掉本句用“/”
+    assetsPublicPath: '/vue2-manage/', //为gh-pages设置，服务器配置可去掉本句用“/”
     productionSourceMap: false,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
